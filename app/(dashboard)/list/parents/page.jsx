2ccf8a2acfb7a94a9043f16f0de37a7e91cf444a
Yared Samuel <yaredcode7@gmail.com>
2024-12-24 02:25:50 -0800

    Starter dahsboard

---
+++ app/(dashboard)/list/parents/page.jsx
@@ -0,0 +1,97 @@
+import Pagination from '@/app/components/Pagination'
+import Table from '@/app/components/Table'
+import TableSearch from '@/app/components/TableSearch'
+import { parentsData, role } from '@/app/lib/data'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+
+const columns = [
+  {
+    header: "Info", accessor: "info"
+  },
+  {
+    header: "Student Name", 
+    accessor: "students", 
+    className: "hidden md:table-cell"
+  },
+  {
+    header: "Phone", 
+    accessor: "phone", 
+    className: "hidden lg:table-cell"
+  },
+  {
+    header: "Address", 
+    accessor: "address", 
+    className: "hidden lg:table-cell"
+  },
+  {
+    header: "Actions", 
+    accessor: "action", 
+    
+  },
+]
+
+const ParentListPage = () => {
+  
+
+
+  const renderRaw = (item) => (
+    <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 text-sm bg-lamaPurpleLight'>
+
+    <td className='flex items-center g-4 p-4'>
+      <div className="flex flex-col">
+        <h3 className='font-semibold'>{item.name}</h3>
+        <p className='text-sm text-gray-500'>{item?.email}</p>
+      </div>
+    </td>
+    <td className='hidden md:table-cell'>{item.students.join(",")}</td>
+    <td className='hidden md:table-cell'>{item.phone}</td>
+    <td className='hidden md:table-cell'>{item.address}</td>
+    <td>
+      <div className="flex items-center gap-2">
+        <Link href={`/list/teachers/${item.id}`}>
+          <button className='w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky'>
+            <Image src="/view.png" alt='' width={16} height={16} />
+          </button>
+        </Link>
+        {role  === "admin" && (<button className='w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple'>
+            <Image src="/view.png" alt='' width={16} height={16} />
+        </button>)}
+      </div>
+    </td>
+  </tr>
+  )
+  return (
+    <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
+      {/* TOP  */}
+      <div className="flex items-center justify-between">
+        <h1 className='hidden md:block text-lg font-semibold'>All Parents</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center gap-4 self-end">
+            <button className='w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow'>
+              <Image src="/filter.png" alt='' width={14} height={14} />
+            </button>
+            <button className='w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow'>
+              <Image src="/sort.png" alt='' width={14} height={14} />
+            </button>
+            {role === "admin" && (
+              <button className='w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow'>
+              <Image src="/plus.png" alt='' width={14} height={14} />
+            </button>)}
+          </div>
+        </div>
+      </div>
+      {/* LIST  */}
+      <Table columns={columns} renderRaw={renderRaw} data={parentsData}/>
+      {/* PAGINATION  */}
+      <div className="">
+        <Pagination />
+      </div>
+    </div>
+  )
+}
+
+export default ParentListPage
---
+++ app/components/forms/TeacherForm.jsx
@@ -0,0 +1,9 @@
+'use client'
+
+const TeacherForm = ({type , data}) => {
+  return (
+    <form className=""></form>
+  )
+}
+
+export default TeacherForm
